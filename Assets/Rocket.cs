8152f40feeea1d98aab4f855fa195c763cebf0be
talesofterror <mich.rivera84@gmail.com>
2020-05-16 20:36:05 -0400

    Simple Flight, Up

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            print("Thrusting");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Rotate Right");
+        }
+    }
+}
