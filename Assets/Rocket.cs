06b78b973172989f98cf482a0958f286b92ab1a5
talesofterror <mich.rivera84@gmail.com>
2020-05-17 00:32:37 -0400

    Added working directional movement. [Space] to thrust and arrow keys to aim the rocket left and right. Left and Right movement is actually a rotation of the Rocket prefab using Unity's "transform" function. "transform.Rotate" specifies that we want the rotate property of the transform function, and the parameters "Vector3.forward" and "Vector3.back" indicate that we want to use Unity's 3D spacial properties (named "Vector3") to go forward or back.
    Center of gravity is still too low, may have to recreate the prefab to adjust the center of gravity. There is also test code here for figuring out how compound operator "+=" works.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,32 +7,56 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
 
+    // was wondering what this value looked like. It just repeatedly prints zero. Add
+    int number = 2;
+    float deltaTimeValue = Time.deltaTime;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        PlusMinus();
+        DeltaPrint();
+    }
+
+    void DeltaPrint()
+    {
+        print(deltaTimeValue);
+        Console.Clear(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        RocketControls();
+        DeltaPrint();
     }
 
-    private void ProcessInput()
+    void PlusMinus()
+    {
+        number = number + 2; // by which I mean: x = x + y
+        print(number);
+    }
+
+    private void RocketControls()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-            print("Thrusting");
         }
+        // Side to side movement. transform 
+        //
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     transform.position += (Vector3.left * Time.deltaTime);
+        // }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            print("Rotate Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            print("Rotate Right");
+            transform.Rotate(Vector3.back);
         }
     }
 }
