0eeb5b4556892032fa381388b54c437a01434a8e
mike <mich.rivera84@gmail.com>
2023-02-19 22:17:17 -0500

    camera transitions

--- Assets/Scripts/ViewShift.cs
+++ Assets/Scripts/ViewShift.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,30 +9,54 @@ public class ViewShift : MonoBehaviour
 
     enum View
     {
-        camDown, 
-        camUp
+        vertView, 
+        stageView,
+        horzView
     };
 
-    View activeView = View.camUp;
+    public enum TriggerType
+    {
+        stagetrigger,
+        upTrigger,
+        downTrigger,
+        leftTrigger,
+        rightTrigger
+    }
+
+
+    // at this point activeView must be set to stageView if starting at top stage
+    // and vertView if starting somewhere within the cave
+    View activeView = View.vertView;
+
+    public TriggerType triggerType;
 
     private void OnTriggerEnter(Collider other)
     {
+
+        print("Entry Collision");
         
         if (other.CompareTag("GuyBase"))
         {
-            if (activeView == View.camUp)
+            if (triggerType == TriggerType.downTrigger)
             {
                 viewerCam.GetComponent<viewer>().ShiftDown();
-                activeView = View.camDown;
+                activeView = View.vertView;
                 print("Entry Trigger, ShiftDown");
             }
 
 
-            else if (activeView == View.camDown)
+            else if (triggerType == TriggerType.stagetrigger) // need a way to trigger this if it's in any other state
             {
                 viewerCam.GetComponent<viewer>().ShiftUp();
-                activeView = View.camUp;
+                activeView = View.stageView;
                 print("Entry Trigger, ShiftUp");
+            }
+
+            else if (triggerType == TriggerType.leftTrigger)
+            {
+                viewerCam.GetComponent<viewer>().ShiftLeft();
+                activeView = View.horzView;
+                print("Entry Trigger, ShiftLeft");
             }
 
         }
--- Assets/Scripts/viewer.cs
+++ Assets/Scripts/viewer.cs
@@ -14,22 +14,28 @@ public class viewer : MonoBehaviour
     public float m_fieldOfView = 0f;
     public float distance = -45f;
     public float xPos = 0f;
+    public float yPos = 0f;
+    public float xPosOffset = 0f;
     public float yPosOffset = 0f;
     public float xRot = 0f;
     public float yRot = 0f;
     public float zRot = 0f;
+    public float panSpeed = 2f;
 
     public float vertMidOffset = 6f;
+    public float horzMidOffset = 6f;
 
     enum CamState
     {
         stageView, 
         downAnim,
         vertMiddle, 
-        upAnim
+        upAnim,
+        leftAnim,
+        horzMiddle
     }
 
-    CamState state = CamState.stageView;
+    CamState state = CamState.vertMiddle;
 
     void Start()
     {
@@ -51,6 +57,11 @@ public class viewer : MonoBehaviour
         state = CamState.upAnim;
     }
 
+    public void ShiftLeft()
+    {
+        state = CamState.leftAnim;
+    }
+
     public void downAnimPlay()
     {
         // transform.position = new Vector3(xPos, playerPos.transform.position.y - yPosOffset + viewShiftAnim, distance);
@@ -58,7 +69,7 @@ public class viewer : MonoBehaviour
         Vector3 startPos = transform.position;
         Vector3 endPos = new Vector3(xPos, playerPos.transform.position.y - yPosOffset + vertMidOffset, distance);
 
-        transform.position = Vector3.Lerp(startPos, endPos, Time.deltaTime * 2f);
+        transform.position = Vector3.Lerp(startPos, endPos, Time.deltaTime * panSpeed);
         if (transform.position == endPos)
         {
             state = CamState.vertMiddle;
@@ -68,40 +79,31 @@ public class viewer : MonoBehaviour
     public void upAnimPlay ()
     {
         Vector3 startPos = transform.position;
-        Vector3 endPos = new Vector3(xPos, playerPos.transform.position.y + yPosOffset, distance);
+        Vector3 endPos = new Vector3(playerPos.transform.position.y -yPosOffset + horzMidOffset, yPos, distance);
 
-        transform.position = Vector3.Lerp(startPos, endPos, Time.deltaTime * 2f);
+        transform.position = Vector3.Lerp(startPos, endPos, Time.deltaTime * panSpeed);
         if (transform.position == endPos)
         {
             state = CamState.stageView;
         }
     }
 
+    public void leftAnimPlay ()
+    {
+        yPos = -40.29f;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(playerPos.transform.position.x - xPosOffset + horzMidOffset, yPos, distance);
+
+        transform.position = Vector3.Lerp(startPos, endPos, Time.deltaTime * panSpeed);
+        if (transform.position == endPos)
+        {
+            state = CamState.horzMiddle;
+        }
+    }
+
     void Update()
     {
-        // float thrustThisFrame = rocketScript.thrustPower * Time.deltaTime;
-
-        // if (Input.GetKey(KeyCode.Space))
-        // {
-        //    camBody.isKinematic = false;
-        //    camBody.AddRelativeForce(Vector3.up * thrustThisFrame);
-        // }
-        // else
-        // {
-        //    camBody.isKinematic = true;
-        // }
-
-        // float rocketPostion = rocketShip.transform.position.y;
-        // camBody.MovePosition(transform.position + rocketPostion);
-
-        // ^ none of this shit worked smh
-
-        /*
-         *       CAMERA POSITION IS ALWAYS OFFSET BY yPosOffset *****
-        */
-
-        
-
+    
         if (state == CamState.stageView)
         {
             transform.position = new Vector3(xPos, playerPos.transform.position.y + yPosOffset, distance);
@@ -123,6 +125,17 @@ public class viewer : MonoBehaviour
         if(state == CamState.upAnim)
         {
             upAnimPlay();
+        }
+
+        if(state == CamState.leftAnim)
+        {
+            leftAnimPlay();
+        }
+
+        if (state == CamState.horzMiddle)
+        {
+            transform.position = new Vector3(playerPos.transform.position.x - xPosOffset + vertMidOffset, yPos, distance);
+            transform.rotation = Quaternion.Euler(xRot, yRot, zRot);
         }
 
     }
