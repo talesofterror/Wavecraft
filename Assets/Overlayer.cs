a147ac78934a535ba5aee2b14949b9f694994a59
talesofterror <mich.rivera84@gmail.com>
2024-02-06 17:14:17 -0500

    minor changes to building colors and fixed ui cursor perspective issue. Mostly checking to see if i can effectively push from vineland and pull in philly

--- Assets/Overlayer.cs
+++ Assets/Overlayer.cs
@@ -6,12 +6,15 @@ public class Overlayer : MonoBehaviour
   public GameObject mainCamObject;
   private viewer mainCamViewer;
   private ViewState currentState;
+
+  private bool swivel = true;
+
   // Start is called before the first frame update
   void Start()
   {
     cam = GetComponent<Camera>();
     mainCamViewer = mainCamObject.GetComponent<viewer>();
-    
+
   }
 
   // Update is called once per frame
@@ -22,10 +25,24 @@ public class Overlayer : MonoBehaviour
     if (currentState == ViewState.stageView)
     {
       cam.orthographic = false;
+      cam.fieldOfView = 28f;
     }
     else
     {
-      cam.orthographic = true;
+      if (Input.GetKeyDown(KeyCode.C)) {
+        if (swivel == true){
+          swivel = false;
+        } else {
+          swivel = true;
+        }
+      }
+      if (swivel == true) {
+        cam.orthographic = true;
+      }
+      if (swivel == false) {
+        cam.orthographic = false;
+        cam.fieldOfView = 44f;
+      }
     }
   }
 }
