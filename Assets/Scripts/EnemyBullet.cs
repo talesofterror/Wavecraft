fb177c60419ad152fd97459baaf9fe2576a4ef5c
mike <mich.rivera84@gmail.com>
2024-04-07 00:01:13 -0400

    should have just napped

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -1,18 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+[ExecuteAlways]
+  public int bulletPoolSize;
+  GameObject[] bulletPool;
+  SphereCollider[] bulletColliders;
+  MeshRenderer[] bulletRenderers;
+  Rigidbody[] bulletRigidBodies;
   public Transform[] spawnPoints;
-  
-    void Start()
-    {
-        
-    }
 
-    void Update()
+  public float detectionDistance;
+
+  public enum ShootStyle
+  {
+    intermittent,
+    nonstop,
+
+  };
+
+  ShootStyle shootStyle = ShootStyle.intermittent;
+
+  void Start()
+  {
+    bulletPool = new GameObject[bulletPoolSize];
+    for (int i = 0; i < bulletPoolSize; i++)
     {
-        
+      bulletPool[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+      bulletPool[i].transform.position = transform.position * 2;
+      bulletPool[i].AddComponent<Rigidbody>();
+      bulletPool[i].name = "bullet " + i;
+      print("bullet generated: #"+i);
+      bulletColliders[i] = bulletPool[i].GetComponent<SphereCollider>();
+      bulletRenderers[i] = bulletPool[i].GetComponent<MeshRenderer>();
+      bulletRigidBodies[i] = bulletPool[i].GetComponent<Rigidbody>();
     }
+  }
+
+  void Update()
+  {
+    if (shootStyle == ShootStyle.intermittent)
+    {
+      StartCoroutine(shootIntermittentIE());
+    }
+  }
+
+  IEnumerator shootIntermittentIE()
+  {
+
+    yield return null;
+  }
 }
