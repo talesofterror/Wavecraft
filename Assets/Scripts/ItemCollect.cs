ed6a63fceca5f11827760b70cdf466f488aa259f
mike <mich.rivera84@gmail.com>
2024-04-14 11:20:32 -0400

    added a collectible item: floppy, along with it's ui element.

---
+++ Assets/Scripts/ItemCollect.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class ItemCollect : MonoBehaviour
+{
+  public GameObject floppyMesh;
+  public ParticleSystem pSystem;
+  Vector3 initialScale;
+  Vector3 targetScaleVector;
+  void Start()
+  {
+    if (pSystem)
+    {
+      pSystem.gameObject.SetActive(false);
+    }
+    initialScale = floppyMesh.transform.localScale;
+    targetScaleVector = new Vector3(0, 0, 0);
+  }
+
+  void OnTriggerEnter(Collider trigger)
+  {
+    StartCoroutine(ScaleDown());
+    if (pSystem)
+    {
+      pSystem.gameObject.SetActive(true);
+      pSystem.Play();
+    }
+  }
+
+  public float shrinkSpeed = 1.5f;
+  float i;
+  IEnumerator ScaleDown()
+  {
+    for (i = 0; i < 1; i += Time.deltaTime * shrinkSpeed)
+    {
+      print("ScaleDown iteration: " + i);
+      transform.localScale = Vector3.Lerp(initialScale, targetScaleVector, i);
+      yield return null;
+    }
+    Destroy(gameObject);
+  }
+  void Update()
+  {
+
+  }
+}
